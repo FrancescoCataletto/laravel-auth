66964548f52bc719fe84a541cc825d08b3b1bc76
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-07-06 15:51:33 +0200

    styled control buttons in page

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,6 +8,7 @@
                 <td>TITLE</td>
                 <td>TEXT</td>
                 <td>SLUG</td>
+                <td>AZIONI</td>
             </tr>
         </thead>
         <tbody>
@@ -16,6 +17,15 @@
                 <td>{{$post->title}}</td>
                 <td>{{$post->text}}</td>
                 <td>{{$post->slug}}</td>
+                <td>
+                    <a href="" class="btn btn-primary">SHOW</a>
+                    <a href="" class="btn btn-success">EDIT</a>
+                    <form action="" method="POST" onsubmit="return confirm('Are you sure you want to delete this post?')">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="DELETE" class="btn btn-danger">
+                    </form>
+                </td>
             </tr>
             @endforeach
         </tbody>
